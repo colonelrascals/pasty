4822d0870176c282aa7c5507ce2da2293c876962
patrick.hildreth <patrick.hildreth@patrick-hildreth-mbp.local>
2018-07-30 11:39:30 -0500

    update struc

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject pasty "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A pastebin clone"
+  :url "https://www.github.com/colonelrascals/pasty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [aleph "0.4.6"]
+                 [hiccup "1.0.5"]
+                 [bidi "2.1.3"]]
   :main ^:skip-aot pasty.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
